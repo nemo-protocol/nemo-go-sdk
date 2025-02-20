1a4d6d7aec2543f244a11479339dcf3793b5c40b
AricRedemption <aricredemption@gmail.com>
2025-02-20 20:51:45 +0800

    feat: Add Cetus swap functionality and update project configuration

--- go.mod
+++ go.mod
@@ -6,11 +6,13 @@ require (
 	github.com/block-vision/sui-go-sdk v1.0.6
 	github.com/coming-chat/go-sui/v2 v2.0.2-0.20240528143942-5eea4e9c30d0
 	github.com/fardream/go-bcs v0.7.0
+	github.com/stretchr/testify v1.10.0
 )
 
 require (
 	github.com/btcsuite/btcutil v1.0.2 // indirect
 	github.com/coming-chat/go-aptos v0.0.0-20221013022715-39f91035c785 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.12.0 // indirect
@@ -18,6 +20,7 @@ require (
 	github.com/leodido/go-urn v1.2.2 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
@@ -27,4 +30,5 @@ require (
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- service/sui/service/swapService.go
+++ service/sui/service/swapService.go
@@ -5,42 +5,43 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/coming-chat/go-sui/v2/account"
-	"github.com/coming-chat/go-sui/v2/sui_types"
-	"github.com/coming-chat/go-sui/v2/types"
-	"github.com/fardream/go-bcs/bcs"
 	"nemo-go-sdk/service/sui/api"
 	"nemo-go-sdk/service/sui/common/constant"
 	"nemo-go-sdk/service/sui/common/models"
+
+	"github.com/coming-chat/go-sui/v2/account"
+	"github.com/coming-chat/go-sui/v2/move_types"
+	"github.com/coming-chat/go-sui/v2/sui_types"
+	"github.com/coming-chat/go-sui/v2/types"
+	"github.com/fardream/go-bcs/bcs"
 )
 
-func (s *SuiService)SwapByPy(amountIn, slippage float64, amountInType, exactAmountOutType string, sender *account.Account, nemoConfig *models.NemoConfig) (bool, error){
+func (s *SuiService) SwapByPy(amountIn, slippage float64, amountInType, exactAmountOutType string, sender *account.Account, nemoConfig *models.NemoConfig) (bool, error) {
 	ptb := sui_types.NewProgrammableTransactionBuilder()
 	suiService := InitSuiService()
 
 	oracleArgument, err := api.GetPriceVoucher(ptb, suiService.SuiApi, nemoConfig)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
 
 	var swapArgument *sui_types.Argument
-	if amountInType == constant.PTTYPE{
+	if amountInType == constant.PTTYPE {
 		swapArgument, err = api.SwapExactPtForSy(ptb, suiService.BlockApi, suiService.SuiApi, nemoConfig, sender.Address, oracleArgument)
-		if err != nil{
+		if err != nil {
 			return false, err
 		}
-	} else if amountInType == constant.YTTYPE{
+	} else if amountInType == constant.YTTYPE {
 		swapArgument, err = api.SwapExactYtForSy(ptb, suiService.BlockApi, suiService.SuiApi, nemoConfig, sender.Address, oracleArgument)
-		if err != nil{
+		if err != nil {
 			return false, err
 		}
 	} else {
 		return false, errors.New("swap type error！")
 	}
 
-
 	syRedeemResult, err := api.SyRedeem(ptb, suiService.SuiApi, nemoConfig, swapArgument)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
 
@@ -83,7 +84,7 @@ func (s *SuiService)SwapByPy(amountIn, slippage float64, amountInType, exactAmou
 	pt := ptb.Finish()
 
 	_, gasCoin, err := api.RemainCoinAndGas(suiService.SuiApi, sender.Address, uint64(10000000), constant.GASCOINTYPE)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
 	gasPayment := []*sui_types.ObjectRef{gasCoin}
@@ -131,75 +132,74 @@ func (s *SuiService)SwapByPy(amountIn, slippage float64, amountInType, exactAmou
 		return false, fmt.Errorf("failed to execute transaction: %w", err)
 	}
 
-	b,_ := json.Marshal(resp)
-	fmt.Printf("\n==resp:%+v==\n",string(b))
+	b, _ := json.Marshal(resp)
+	fmt.Printf("\n==resp:%+v==\n", string(b))
 
 	return false, nil
 }
 
-func (s *SuiService)SwapToPy(amountIn, slippage float64, amountInType, exactAmountOutType string, sender *account.Account, nemoConfig *models.NemoConfig) (bool, error){
+func (s *SuiService) SwapToPy(amountIn, slippage float64, amountInType, exactAmountOutType string, sender *account.Account, nemoConfig *models.NemoConfig) (bool, error) {
 	client := InitSuiService()
-	netSyIn := uint64(amountIn*1000000000)
+	netSyIn := uint64(amountIn * 1000000000)
 
 	minYtOut, err := api.DryRunGetPyOutForExactSyInWithPriceVoucher(client.SuiApi, nemoConfig, exactAmountOutType, netSyIn, nemoConfig.PriceOracle, sender)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
-	minYtOut = minYtOut - uint64(float64(minYtOut) * slippage)
+	minYtOut = minYtOut - uint64(float64(minYtOut)*slippage)
 
 	approxPyOut, netSyTokenization, err := api.DryRunGetApproxPyOutForNetSyInInternal(client.SuiApi, nemoConfig, exactAmountOutType, netSyIn, minYtOut, sender)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
 
 	ptb := sui_types.NewProgrammableTransactionBuilder()
-	
+
 	remainingCoins, gasCoin, err := api.RemainCoinAndGas(client.SuiApi, sender.Address, uint64(10000000), amountInType)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
-	
-	if !constant.IsGasCoinType(nemoConfig.CoinType){
+
+	if !constant.IsGasCoinType(nemoConfig.CoinType) {
 		_, gasCoin, err = api.RemainCoinAndGas(client.SuiApi, sender.Address, uint64(10000000), constant.GASCOINTYPE)
-		if err != nil{
+		if err != nil {
 			return false, err
 		}
 	}
 
 	mergeCoinArgument, remainingCoins, err := api.MergeCoin(ptb, client.SuiApi, remainingCoins, netSyIn)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
 
-	splitResult,_,err := api.SplitCoinFromMerged(ptb, *mergeCoinArgument[0], netSyIn)
-	if err != nil{
+	splitResult, _, err := api.SplitCoinFromMerged(ptb, *mergeCoinArgument[0], netSyIn)
+	if err != nil {
 		return false, err
 	}
 
 	depositArgument, err := api.Deposit(ptb, client.SuiApi, nemoConfig, &splitResult)
-	if err != nil{
-		return false, err
-	}
-	
-	oracleArgument, err := api.GetPriceVoucher(ptb, client.SuiApi, nemoConfig)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
 
-	if exactAmountOutType == constant.YTTYPE{
+	oracleArgument, err := api.GetPriceVoucher(ptb, client.SuiApi, nemoConfig)
+	if err != nil {
+		return false, err
+	}
+
+	if exactAmountOutType == constant.YTTYPE {
 		_, err = api.SwapExactSyForYt(ptb, client.BlockApi, client.SuiApi, nemoConfig, sender.Address, approxPyOut, netSyTokenization, minYtOut, oracleArgument, depositArgument)
-		if err != nil{
+		if err != nil {
 			return false, err
 		}
-	}else if exactAmountOutType == constant.PTTYPE{
+	} else if exactAmountOutType == constant.PTTYPE {
 		_, err = api.SwapExactSyForPt(ptb, client.BlockApi, client.SuiApi, nemoConfig, sender.Address, approxPyOut, minYtOut, oracleArgument, depositArgument)
-		if err != nil{
+		if err != nil {
 			return false, err
 		}
-	}else{
+	} else {
 		return false, errors.New("swap type error！")
 	}
-
 
 	// transfer object
 	//transferArgs := []sui_types.Argument{remainMergeCoinArgument}
@@ -229,8 +229,6 @@ func (s *SuiService)SwapToPy(amountIn, slippage float64, amountInType, exactAmou
 	pt := ptb.Finish()
 
 	gasPayment := []*sui_types.ObjectRef{gasCoin}
-
-
 
 	tx := sui_types.NewProgrammable(
 		*senderAddr,
@@ -270,8 +268,178 @@ func (s *SuiService)SwapToPy(amountIn, slippage float64, amountInType, exactAmou
 		return false, fmt.Errorf("failed to execute transaction: %w", err)
 	}
 
-	b,_ := json.Marshal(resp)
-	fmt.Printf("\n==resp:%+v==\n",string(b))
+	b, _ := json.Marshal(resp)
+	fmt.Printf("\n==resp:%+v==\n", string(b))
 
 	return false, nil
+}
+
+func (s *SuiService) SwapCetusB2A(amountIn float64, sender *account.Account) (bool, error) {
+	netAmountIn := uint64(amountIn * 1000000000)
+	// create trade builder
+	ptb := sui_types.NewProgrammableTransactionBuilder()
+	client := InitSuiService()
+
+	remainingCoins, gasCoin, err := api.RemainCoinAndGas(client.SuiApi, sender.Address, uint64(10000000), "0x2::sui::SUI")
+	if err != nil {
+		return false, err
+	}
+
+	if constant.IsGasCoinType("0x2::sui::SUI") {
+		_, gasCoin, err = api.RemainCoinAndGas(client.SuiApi, sender.Address, uint64(10000000), constant.GASCOINTYPE)
+		if err != nil {
+			return false, err
+		}
+	}
+
+	mergeCoinArgument, remainingCoins, err := api.MergeCoin(ptb, client.SuiApi, remainingCoins, netAmountIn)
+	if err != nil {
+		return false, err
+	}
+
+	splitResult, _, err := api.SplitCoinFromMerged(ptb, *mergeCoinArgument[0], netAmountIn)
+	if err != nil {
+		return false, err
+	}
+
+	// Get object arguments for the swap
+	globalConfigArg, err := api.GetObjectArgument(ptb, client.SuiApi, "0xdaa46292632c3c4d8f31f23ea0f9b36a28ff3677e9684980e4438403a67a3d8f", false, "0x1eabed72c53feb3805120a081dc15963c204dc8d091542592abaf7a35689b2fb", "config", "swap_b2a")
+	if err != nil {
+		return false, err
+	}
+
+	poolArg, err := api.GetObjectArgument(ptb, client.SuiApi, "0x6c545e78638c8c1db7a48b282bb8ca79da107993fcb185f75cedc1f5adb2f535", true, "0x1eabed72c53feb3805120a081dc15963c204dc8d091542592abaf7a35689b2fb", "pool", "swap_b2a")
+	if err != nil {
+		return false, err
+	}
+
+	partnerArg, err := api.GetObjectArgument(ptb, client.SuiApi, "0xeb863165a109f7791a3182be08aff1438ab2a429314fc135ae19d953afe1edd6", true, "0x1eabed72c53feb3805120a081dc15963c204dc8d091542592abaf7a35689b2fb", "partner", "swap_b2a")
+	if err != nil {
+		return false, err
+	}
+
+	clockArg, err := api.GetObjectArgument(ptb, client.SuiApi, "0x0000000000000000000000000000000000000000000000000000000000000006", false, "0x2", "clock", "swap_b2a")
+	if err != nil {
+		return false, err
+	}
+
+	// Convert contract address to ObjectID
+	contractAddr, err := sui_types.NewObjectIdFromHex("0x2485feb9d42c7c3bcb8ecde555ad40f1b073d9fb4faf354fa2d30a0b183a23ce")
+	if err != nil {
+		return false, err
+	}
+
+	// Create type arguments
+	usdcStructTag, err := api.GetStructTag("0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC")
+	if err != nil {
+		return false, err
+	}
+	usdcTypeTag := move_types.TypeTag{
+		Struct: usdcStructTag,
+	}
+
+	suiStructTag, err := api.GetStructTag("0x2::sui::SUI")
+	if err != nil {
+		return false, err
+	}
+	suiTypeTag := move_types.TypeTag{
+		Struct: suiStructTag,
+	}
+
+	typeArguments := make([]move_types.TypeTag, 0)
+	typeArguments = append(typeArguments, usdcTypeTag) // T0
+	typeArguments = append(typeArguments, suiTypeTag)  // T1
+
+	// Call swap_b2a
+	swapResult := ptb.Command(sui_types.Command{
+		MoveCall: &sui_types.ProgrammableMoveCall{
+			Package:       *contractAddr,
+			Module:        "cetus",
+			Function:      "swap_b2a",
+			TypeArguments: typeArguments,
+			Arguments: []sui_types.Argument{
+				globalConfigArg,
+				poolArg,
+				partnerArg,
+				splitResult,
+				clockArg,
+			},
+		},
+	})
+
+	// Transfer the result to sender
+	recipientAddr, err := sui_types.NewAddressFromHex(sender.Address)
+	if err != nil {
+		return false, err
+	}
+
+	recArg, err := ptb.Pure(*recipientAddr)
+	if err != nil {
+		return false, err
+	}
+
+	transferArgs := []sui_types.Argument{swapResult}
+	ptb.Command(
+		sui_types.Command{
+			TransferObjects: &struct {
+				Arguments []sui_types.Argument
+				Argument  sui_types.Argument
+			}{
+				Arguments: transferArgs,
+				Argument:  recArg,
+			},
+		},
+	)
+
+	pt := ptb.Finish()
+
+	gasPayment := []*sui_types.ObjectRef{gasCoin}
+
+	senderAddr, err := sui_types.NewObjectIdFromHex(sender.Address)
+	if err != nil {
+		return false, fmt.Errorf("failed to convert sender address: %w", err)
+	}
+
+	tx := sui_types.NewProgrammable(
+		*senderAddr,
+		gasPayment,
+		pt,
+		10000000, // gasBudget
+		1000,     // gasPrice
+	)
+
+	txBytes, err := bcs.Marshal(tx)
+	if err != nil {
+		return false, fmt.Errorf("failed to serialize transaction: %w", err)
+	}
+
+	// signature
+	signature, err := sender.SignSecureWithoutEncode(txBytes, sui_types.DefaultIntent())
+	if err != nil {
+		return false, fmt.Errorf("failed to sign transaction: %w", err)
+	}
+
+	options := types.SuiTransactionBlockResponseOptions{
+		ShowInput:          true,
+		ShowEffects:        true,
+		ShowEvents:         true,
+		ShowObjectChanges:  true,
+		ShowBalanceChanges: true,
+	}
+
+	resp, err := client.SuiApi.ExecuteTransactionBlock(
+		context.Background(),
+		txBytes,
+		[]any{signature},
+		&options,
+		types.TxnRequestTypeWaitForLocalExecution,
+	)
+	if err != nil {
+		return false, fmt.Errorf("failed to execute transaction: %w", err)
+	}
+
+	b, _ := json.Marshal(resp)
+	fmt.Printf("\n==resp:%+v==\n", string(b))
+
+	return true, nil
 }
